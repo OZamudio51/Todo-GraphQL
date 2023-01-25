2a58ccf0885fbe082c7c5f3b95f770d244802220
Oscar Zamudio <zamudio_1526@hotmail.com>
2023-01-24 22:42:39 -0700

    finishing touches for exercise

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const authLink = setContext((_, { headers }) => {
+ return {
+ headers: {
+      ...headers, 'x-hasura-admin-secret': `${process.env.REACT_APP_API_KEY}`
+    }
+  }
+});
+
+const httpLink = createHttpLink({
+  uri: 'https://expert-mammoth-86.hasura.app/v1/graphql',
+ });
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache({
+    typePolicies: {
+      GET_TODOS: {
+        fields: {
+         id : {
+            // shorthand  
+            merge: true,
+          },
+        },
+      },
+    },
+  })
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
